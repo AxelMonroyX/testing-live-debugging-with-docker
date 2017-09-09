61d5e2247531d191bfff03dd3cd7940edc5705bc
Axel Monroy <xaxelmonroyx@gmail.com>
2017-09-08 20:21:11 -0700

    Add test

---
+++ test/index.js
@@ -0,0 +1,24 @@
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
+
+chai.use(chaiHttp);
+describe('Routes', () => {
+
+    /*
+      * Test the /GET route
+      */
+    describe('/GET index', () => {
+        it('it should GET an html', (done) => {
+            chai.request(server)
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+    });
+
+});
